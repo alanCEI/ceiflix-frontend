c0b5431fbd65d24101abc66235f1aba26c0d35a1
alanCEI <alansmallpage9428@alumnos.cei.es>
2025-06-26 19:03:05 +0200

    n

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
-/*const API_BASE_URL = "https://api.themoviedb.org/3";
+const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_TOKEN = import.meta.env.VITE_TOKEN_API_TMDB;
-const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/";*/
+const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/";
 
-const baseURL = import.meta.env.VITE_TOKEN_API_TMDB;
+/*const baseURL = import.meta.env.VITE_TOKEN_API_TMDB;
 
 export const apiClient = {
   get: async (endpoint) => {
@@ -34,10 +34,10 @@ const getHeaders = () => {
     headers['Authorization'] = `Bearer ${token}`;
   }
   return headers;
-};
+};*/
 
 
-/*const fetchFromAPI = async (endpoint, options = {}) => {
+const fetchFromAPI = async (endpoint, options = {}) => {
   if (!API_TOKEN || API_TOKEN === "undefined") {
     throw new Error("El token de API de TMDB no está configurado.");
   }
@@ -90,4 +90,4 @@ export const getPopularMovies = async (options) => {
 export const getMovieDetails = async (movieId, options) => {
   const data = await fetchFromAPI(`movie/${movieId}?language=es-ES`, options);
   return processMovieData(data);
-};*/
+};
